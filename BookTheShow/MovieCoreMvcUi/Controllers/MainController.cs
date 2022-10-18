9cfbbc4097831dcf6c574bad41d916e988297a5c
Chandan M <Chandan.M@valtech.com>
2022-10-18 18:30:05 +0530

    h

---
+++ BookTheShow/MovieCoreMvcUi/Controllers/MainController.cs
@@ -0,0 +1,111 @@
+﻿using BookTheShowEntity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCoreMvcUi.Controllers
+{
+    public class MainController : Controller
+    {
+        IConfiguration _configuration;
+        public MainController(IConfiguration configuration)
+        {
+
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult>Index(Userv userv)
+        {
+            ViewBag.status = "";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(userv), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "User/Login";
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        return RedirectToAction("Index", "Home");
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong credentials!";
+                    }
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Login(Userv userv)
+        {
+            ViewBag.status = "";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(userv), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "User/Login";
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                        return RedirectToAction("Index", "Home");
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong credentials!";
+                    }
+                }
+            }
+            return View();
+
+
+
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Register(Userv userv)
+        {
+            ViewBag.status = "";
+            //using grabage collection only for inbuilt classes
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(userv), Encoding.UTF8, "application/json");
+                string endPoint = _configuration["WebApiBaseUrl"] + "User/Register";//api controller name and its function
+
+                using (var response = await client.PostAsync(endPoint, content))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {   //dynamic viewbag we can create any variable name in run time
+                        ViewBag.status = "Ok";
+                        ViewBag.message = "User Details Saved Successfull!!";
+                    }
+
+                    else
+                    {
+                        ViewBag.status = "Error";
+                        ViewBag.message = "Wrong Entries";
+                    }
+
+                }
+            }
+            return View();
+        }
+
+    }
+}
